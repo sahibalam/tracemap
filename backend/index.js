93e84ecadcaabcce0c8783cf0b5eaf4d3529f7ee
Sahib Alam <sahib.kolkata@gmail.com>
2026-07-01 21:50:31 +0530

    Add worker wizard API and upload endpointsss

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,4 @@
-// import dotenv from "dotenv";
-// dotenv.config();
 
-// import express from "express";
-// import cors from "cors";
-// import authRoutes from "./src/routes/authRoutes.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-
-// app.use(express.json());
-// app.use((req, res, next) => {
-//     console.log(req.method, req.url);
-//     next();
-//   });
-
-// app.use(
-//   "/api/auth",
-//   authRoutes
-// );
-// const PORT =
-//   process.env.PORT || 5001;
-
-// app.listen(PORT, () => {
-
-//   console.log(
-//     `Server running on ${PORT}`
-//   );
-
-// });
-
-
-// backend/src/index.js
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -63,9 +28,7 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
-
       if (
         allowedOrigins.indexOf(origin) !== -1 ||
         process.env.NODE_ENV === "development"
